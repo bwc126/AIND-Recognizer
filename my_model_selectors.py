0860631f1ab8006408759049ae150ea9ee97dc92
Brian Coe <bwc126@gmail.com>
2017-05-13 23:26:13 -0400

    feat: Revise for second submission

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -75,28 +75,40 @@ class SelectorBIC(ModelSelector):
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        # Initialize some objects that will be used later.
         best_model = object
         # cur_model = object
         best_score = float('inf')
         word_sequences = self.sequences
         num_samples = len(word_sequences)
-        num_splits = max(min(len(word_sequences), 3), 2)
+        # We need to restrict the number of Kfold splits to [2,3]
+        # num_splits = max(min(len(word_sequences), 3), 2)
+        # For each possible number of states in the model, we'll see how it performs
         for n_components in range(self.min_n_components, self.max_n_components+1):
-            split_method = KFold(num_splits)
+            m = n_components
+            f = self.X.shape[1]
+            # split_method = KFold(num_splits)
             cur_scores = []
+            # Use try/except to handle non-viability of some models
             try:
-                for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
-                    train_x, train_lengths = combine_sequences(cv_train_idx, word_sequences)
-                    test_x, test_lengths = combine_sequences(cv_test_idx, word_sequences)
-                    cur_model = GaussianHMM(n_components).fit(train_x, train_lengths)
-                    logL = cur_model.score(test_x, test_lengths)
-                    BIC = -2 * logL + n_components * np.log(num_samples)
-                    cur_scores.append(BIC)
+                # # We use the average score of several trials across multiple K-fold splits
+                # for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
+                # Combine our training sequences
+                # train_x, train_lengths = combine_sequences(cv_train_idx, word_sequences)
+                # Combine our testing sequences
+                # test_x, test_lengths = combine_sequences(cv_test_idx, word_sequences)
+                # The current model we're testing
+                cur_model = GaussianHMM(n_components).fit(self.X, self.lengths)
+                # Calculate its score
+                logL = cur_model.score(self.X, self.lengths)
+                BIC = -2 * logL + np.log(num_samples) * (m**2 + 2*m*f - 1)
+                # Track the scores for this model, for each fold
+                cur_score = BIC
             except:
                 break
-
-            if np.mean(cur_scores) < best_score:
-                best_score = np.mean(cur_scores)
+            # If the current model sets a new record for performance, it's our new best model
+            if cur_score < best_score:
+                best_score = cur_score
                 best_model = cur_model
         return best_model
 
@@ -111,35 +123,50 @@ class SelectorDIC(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        # Initialize some objects that will be used later.
         best_model = self.base_model(self.min_n_components)
         best_score = float('inf')
         word_sequences = self.sequences
         num_samples = len(word_sequences)
-        num_splits = max(min(len(word_sequences), 3), 2)
-        split_method = KFold(num_splits)
+        # cur_model = object
+        # We need to restrict the number of Kfold splits to [2,3]
+        # num_splits = max(min(len(word_sequences), 3), 2)
+        # split_method = KFold(num_splits)
         M = len(self.hwords)
+        # For each possible number of states in the model, we'll see how it performs
         for n_components in range(self.min_n_components, self.max_n_components+1):
             cur_scores = []
+            # Use try/except to handle non-viability of some models
             try:
-                for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
-                    train_x, train_lengths = combine_sequences(cv_train_idx, word_sequences)
-                    test_x, test_lengths = combine_sequences(cv_test_idx, word_sequences)
-                    cur_model = GaussianHMM(n_components).fit(train_x, train_lengths)
-                    #logP(original word) - 1 / (num words -1) * sum (logP(all other words))
-                    logP = cur_model.score(test_x, test_lengths)
-                    anti_likelihoods = 0.0
-                    words = [word for word in self.words if word != self.this_word]
-                    for word in words:
-                        word_x, word_lengths = self.hwords[word]
-                        anti_likelihoods += cur_model.score(word_x, word_lengths)
-                    avg_anti_likelihood = anti_likelihoods / (M-1)
-                    DIC = logP - avg_anti_likelihood
-                    cur_scores.append(DIC)
+                # We use the average score of several trials across multiple K-fold splits
+                # for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
+                # Combine our training sequences
+                # train_x, train_lengths = combine_sequences(cv_train_idx, word_sequences)
+                # Combine our testing sequences
+                # test_x, test_lengths = combine_sequences(cv_test_idx, word_sequences)
+                # The current model we're testing
+                cur_model = GaussianHMM(n_components).fit(self.X, self.lengths)
+                #logP(original word) - 1 / (num words -1) * sum (logP(all other words))
+                logP = cur_model.score(self.X, self.lengths)
+                # We need to sum the error for the words that are ostensibly not the ones we're training for, and compare how well the model discriminates between its target word and others.
+                anti_likelihoods = 0.0
+                words = [word for word in self.words if word != self.this_word]
+                # Here's where we actually see how well it identifies the other words
+                for word in words:
+                    word_x, word_lengths = self.hwords[word]
+                    anti_likelihoods += cur_model.score(word_x, word_lengths)
+                                        # Calculate its score
+                avg_anti_likelihood = anti_likelihoods / (M-1)
+                # Calculate the difference between the model's ability to identify its target word compared to its ability to identify other words
+                DIC = logP - avg_anti_likelihood
+                # Track the scores for this model, for each fold
+                cur_score = DIC
             except:
                 break
 
-            if np.mean(cur_scores) < best_score:
-                best_score = np.mean(cur_scores)
+            # If the current model sets a new record for performance, it's our new best model
+            if cur_score < best_score:
+                best_score = cur_score
                 best_model = cur_model
         return best_model
 
@@ -150,23 +177,33 @@ class SelectorCV(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        # Initialize some objects that will be used later.
         best_model = object
         cur_model = object
         best_score = float('-inf')
         word_sequences = self.sequences
+        # We need to restrict the number of Kfold splits to [2,3]
+        num_splits = max(min(len(word_sequences), 3), 2)
+        # For each possible number of states in the model, we'll see how it performs
         for n_components in range(self.min_n_components, self.max_n_components+1):
-            num_splits = max(min(len(word_sequences), 3), 2)
             split_method = KFold(num_splits)
             cur_scores = []
+        # Use try/except to handle non-viability of some models
             try:
+            # We use the average score of several trials across multiple K-fold splits
                 for cv_train_idx, cv_test_idx in split_method.split(word_sequences):
-                    train_x, train_lengths = combine_sequences(cv_train_idx, word_sequences)
+                    # Combine our training sequences
                     test_x, test_lengths = combine_sequences(cv_test_idx, word_sequences)
+                    # Combine our testing sequences
+                    train_x, train_lengths = combine_sequences(cv_train_idx, word_sequences)
+                    # The current model we're testing
                     cur_model = GaussianHMM(n_components).fit(train_x, train_lengths)
+                    # Track the scores for this model, for each fold
                     cur_scores.append(cur_model.score(test_x, test_lengths))
             except:
                 break
 
+            # If the current model sets a new record for performance, it's our new best model
             if np.mean(cur_scores) > best_score:
                 best_score = np.mean(cur_scores)
                 best_model = cur_model
--- my_recognizer.py
+++ my_recognizer.py
@@ -18,27 +18,36 @@ def recognize(models: dict, test_set: SinglesData):
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
+    # Initialize our return objects
     probabilities = []
     guesses = []
+    # We need to recognize each word in the test set
     for word_index, test_word in enumerate(test_set.wordlist):
+        # We retrieve the test data for each word in the test set
         test_x, test_lengths = test_set.get_item_Xlengths(word_index)
+        # We prepare to store the predicted probabilities for each word using the model trained for our target word, as well as the score and best guess.
         prob_dict = {}
         scores = []
         guess = None
         best_score = float('-inf')
+        # We try each model to see what word it predicts, what its best guess is, as well as the probability it gives for each word
         for word in models:
             model = models[word]
+            # Handle non-viable models with try/except
             try:
+                # We score this model's ability to detect the target word
                 score = model.score(test_x, test_lengths)
                 scores.append(score)
             except:
                 score = float('-inf')
                 scores.append(score)
+            # We store this model's probability for identifying this word
             prob_dict[word] = score
+            # If we have a new best score, this model has more certainty than any previous model in identifying this word, and we have a new best guess for what the word is.
             if score > best_score:
                 best_score = score
                 guess = word
-
+        # We store our predicted probabilities for each word occuring according to each model, and all of our best guesses. 
         probabilities.append(prob_dict)
         guesses.append(guess)
 
